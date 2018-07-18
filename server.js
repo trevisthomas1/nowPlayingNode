b3059931e1d0934af8251ce00c7b285fb4778cc6
Trevis Thomas <trevislynnthomas@gmail.com>
2018-07-17 21:22:30 -0600

    CSS Fix

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var PORT = process.env.PORT || 3000;
 
 require("./htmlRoutes")(app);
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT)
